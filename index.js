540a245aa091c192e6a72d93655146d0d22681f7
Patrick Coral <patrick.coral@hes-so.ch>
2026-05-07 22:27:37 +0200

    First post

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ var menu
 
 $(async function () {
     menu = document.getElementById("menu")
-
-    let lorem = document.getElementById("lorem")
-    const url = "https://lorem-api.com/api/lorem"
-    const response = await fetch(url)
-    if (response.ok) {
-        const result = await response.text()
-
-        lorem.innerText = result
-    }
 })
 
 function openMenu() {
